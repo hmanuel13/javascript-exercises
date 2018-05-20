2d08a85c11d8e0215038df14c05e42a2d78432ba
Yayan <hilarymanuel@gmail.com>
2018-05-19 22:28:41 -0400

    objects and references

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -3,3 +3,13 @@ let myAccount = {
   expenses: 0,
   income: 0
 }
+
+let otherAccount = myAccount
+otherAccount.income = 1000
+
+let addExpense = function (account, amount){
+  account.expenses += amount
+}
+
+addExpense(myAccount,2.50)
+console.log(myAccount)
