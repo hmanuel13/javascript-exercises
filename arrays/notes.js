619565ae62d4e31e0382f34c1544d494947a8e15
Yayan <hilarymanuel@gmail.com>
2018-05-22 10:37:11 -0400

    array manipulation update

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,6 +1,21 @@
+
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
-console.log(notes.length)
-console.log(notes[1].length)
-console.log(notes[notes.length-1])
-
+console.log(notes)
+notes.pop()
+notes.push('new note')
+console.log(notes)
+notes.shift()
+console.log(notes)
+notes.unshift('unshifted note')
+console.log(notes)
+notes.push('mn1','mn 2','mn3')
+console.log(notes)
+console.log(`note length: ${notes.length}`)
+notes.splice(1,1)
+console.log('splicing at index 1, removing 1')
+console.log(`note length: ${notes.length}`)
+console.log(notes)
+console.log('adding a new item by splicing')
+notes.splice(1,0,'spliced in note')
+console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,3 +1,16 @@
 // create an array with five todos
 // you have x todos
-// 
+// print the first and second to last items -> eg. Todo: walk the dog
+
+const todo = [
+  'design a new website',
+  'create a new design palette',
+  'study javascript algorithms',
+  'study bash scripting',
+  'study rust programming',
+  'discover adobe xd',
+  'study bootstrap'
+]
+
+console.log(`\nYou have ${todo.length} ${(todo.length == 1 ? 'item' : 'items')} to do.`)
+console.log(`first item: ${todo[0]} \nsecond to last item: ${todo[todo.length-2]}`)
