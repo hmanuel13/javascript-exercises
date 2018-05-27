19642b4a05e6589da305cbdfa55437b219a38174
Yayan <hilarymanuel@gmail.com>
2018-05-26 21:48:07 -0400

    not much progress here

--- experiments/kt.js
+++ experiments/kt.js
@@ -20,37 +20,7 @@ console.log(`\ninitial location is: (${k.x},${k.y})`)
 const board = []
 board.push(k)
 
-let isOutOfBounds = function(q){
 
-  if(q.z < 64) return true
-  return false
-}
-
-let isOccupied = function(r){
-  
-  return false
-}
-
-// check if location is valid or input a new one
-let getNext = function(q){
-  
-  let temp = board[board.length-1]
-
-  while(isOutOfBounds(temp) || isOccupied(temp)){
-      switch(temp.z){
-      case 1: temp.x+=2; temp.y+=1; temp.z +=1; break
-      case 2: temp.x+=1; temp.y+=2; temp.z +=1; break
-      case 3: temp.x-=1; temp.y+=2; temp.z +=1; break
-      case 4: temp.x-=2; temp.y+=1; temp.z +=1; break
-      case 5: temp.x-=2; temp.y-=1; temp.z +=1; break
-      case 6: temp.x-=1; temp.y-=2; temp.z +=1; break
-      case 7: temp.x+=1; temp.y-=2; temp.z +=1; break
-      case 8: temp.x+=2; temp.y-=1; temp.z +=1; break
-      default: temp.z = 1; console.log(temp)
-    }
-  }
-  return temp
-}
 
 while(board.length<64){
   k = getNext(k)
